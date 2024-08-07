e0416a3134e8b6639f3821b6d4c172d65c5c38b5
Yonatan Hershko <yonher8@gmail.com>
2024-08-07 13:39:28 +0300

    basic starter

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RootCmp from './RootCmp.jsx'
+import './assets/styles/App.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <RootCmp />
+  </>
+)
